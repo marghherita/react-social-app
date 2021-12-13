51ad40200d28603afea5048fb78c68b5160d7d4a
Margherita Grasso <margherita.grasso3@gmail.com>
2021-12-13 23:25:06 +0100

    feat: lezione 13-12-21

---
+++ src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import styles from './Footer.module.scss';
+
+const Footer = () => {
+
+const year = new Date().getFullYear();
+
+    return (
+        <footer className={styles.footer}>
+            <hr />
+            <p className={styles.red} >Copyright Edgemony {year}</p>
+        </footer>
+    )
+}
+
+export default Footer;
