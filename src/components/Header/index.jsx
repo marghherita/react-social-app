9a67371e3ef67f75343a635bd07e626895501953
Margherita Grasso <margherita.grasso3@gmail.com>
2021-12-20 18:34:01 +0100

    feat: add create new post section

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,18 @@
 
+import { Link, useMatch, useResolvedPath } from 'react-router-dom';
 import styles from "./../Header/Header.module.scss";
 
 const Header = (props) => {
     const name = props.name || 'App';
     const links = props.links || [{ link: "", label: "Link"}];
+
+const CheckActive = (link) => {
+  const resolved = useResolvedPath(link);
+
+  const match = useMatch ({ path: resolved.pathname, end: true });
+
+  return match ? styles.active : "";
+}
 
   return (
     <header className={styles.header}>
@@ -12,7 +21,7 @@ const Header = (props) => {
         <ul>
           {links.map((item, index) => (
             <li key={index}>
-              <a href={item.link}>{item.label}</a>
+              <Link className={CheckActive(item.link)} to={item.link}>{item.label}</Link>
             </li>
           ))}
         </ul>
--- src/libs/http.js
+++ src/libs/http.js
@@ -4,4 +4,13 @@ const BASE_URL = "https://edgemony-backend.herokuapp.com/";
 const http = (resource) => fetch(BASE_URL + resource)
 .then(res => res.json());
 
-export { http };
+const httpPOST = (resource, data) =>
+  fetch(BASE_URL + resource, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(data),
+  }).then((response) => response.json());
+
+export { http, httpPOST };
